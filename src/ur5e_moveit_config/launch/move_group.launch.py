60466077bce3208eeff82f317e5e9b6add19fce9
manelpuig <manel.puig@ub.edu>
2026-06-06 10:51:57 +0000

    added ur5e_moveit_config

---
+++ src/ur5e_moveit_config/launch/move_group.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    declare_use_sim_time = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true",
+        description="Use simulation time",
+    )
+
+    moveit_config = (
+        MoveItConfigsBuilder("my_arm", package_name="ur5e_moveit_config")
+        .to_moveit_configs()
+    )
+
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"use_sim_time": use_sim_time},
+        ],
+    )
+
+    return LaunchDescription([
+        declare_use_sim_time,
+        move_group_node,
+    ])
